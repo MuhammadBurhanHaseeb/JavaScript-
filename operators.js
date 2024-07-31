beed57ae2132ac2828bf442b1900e41d157f738e
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-07-31 20:09:21 +0500

    Operations

---
+++ operators.js
@@ -0,0 +1,64 @@
+//airthmetic operators
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log(("a+b"),c);
+c = a-b;
+console.log(("a-b"),c);
+c = a*b;
+console.log(("a*b"),c);
+c = a/b;
+console.log(("a/b"),c);
+c = a%b;
+console.log(("a%b"),c);
+c = a**b;
+console.log(("a**b"),c);
+
+
+console.log(a--);
+console.log(--b);
+console.log(a++);
+console.log(++a);
+
+//assignment operators
+
+
+console.log("Assignment operators");
+let xx = 10;
+xx += 10;
+console.log(xx);
+xx -= 10;
+console.log(xx);
+
+xx *= 10;
+console.log(xx);
+xx /= 10;
+console.log(xx);
+xx %= 10;
+console.log(xx);
+xx **= 10;
+console.log(xx);
+
+//comparison operators
+console.log("comparison operators");
+let aa = 10;
+let bb = 20;
+let aaa="10";
+console.log(aa==bb);
+console.log(aa!=bb);
+console.log(aa>bb);
+console.log(aa<bb);
+console.log(aa>=bb);
+console.log(aa<=bb);
+console.log(aa===aaa);//false 
+console.log(aa!==aaa);//true
+console.log(aa==aaa);//true
+
+//logical operators
+console.log("logical operators");
+let xxx = true;
+let yyy = false;
+console.log(xxx&&yyy);
+console.log(xxx||yyy);
+console.log(!xxx);
+
