04e58ea4aee6c8879c9d384d03753938b8966553
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-07-31 20:07:27 +0500

    Variables

---
+++ Variables-DataTypes.js
@@ -0,0 +1,27 @@
+console.log("hello how are you ");//to print any thing
+name ="tonny stark";
+console.log(name);
+x=null;
+console.log(x);
+y=undefined;
+console.log(y);
+isFalse=false;
+console.log(isFalse);
+
+let namee ="tonny stark";
+console.log(namee);
+let x=90;
+console.log(x);
+// const y=10;
+// y=66;
+// console.log(y);  
+BigInt  = 3434355n;
+console.log(BigInt);
+const student= {
+    name:"tonny",
+    age:20,
+    isMale:true
+};
+console.log(student);
+console.log(student.name);
+console.log(student["name"]);
