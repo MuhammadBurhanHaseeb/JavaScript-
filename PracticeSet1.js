388a7d13fdb3f3bed46df79d3aa045a8f3dbb7fa
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-07-31 20:11:24 +0500

    PracticeSet1

---
+++ PracticeSet1.js
@@ -0,0 +1,24 @@
+
+//q1
+const products = 
+{
+    name :"tutut",
+    price : "1000",
+    isAvailable:true ,
+    quantity : 10,
+    isDiscounted:false  
+};
+console.log(products);
+
+//q2
+const profile ={
+    name :"BHB",
+    posts: 100,
+    followers: 1000,
+    following : 4,
+    isVerified : true,
+    bio : "I am a good person",
+    image : "image.jpg"
+}
+console.log(profile);
+console.log(typeof profile["name"]);
