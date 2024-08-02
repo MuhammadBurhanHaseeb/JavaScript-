6c09d536f193c399bdc49cc51e2c9b8adfc16a5a
Muhammad Burhan <muhammadburhanhaseeb@gmail.com>
2024-08-02 11:52:35 +0500

    Error Handling

---
+++ errorHandling.js
@@ -0,0 +1,16 @@
+
+let a = 10;
+let b = 30;
+try{
+    console.log("a = ", a );
+    console.log("b = ", b );
+    console.log("a + b = ", a + b +c);
+  
+   
+}catch(e){
+    console.log("yt");
+
+}
+console.log("a * b = ", a * b);
+console.log("a / b = ", a / b);
+
